ba28887b97caa3885ef710038ca74ce6076a0dd8
christeluwgb <christel@uwgb.edu>
2020-09-21 17:42:50 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  
+  void* void_pointer = nullptr;
+
+  std::cout << "void_pointer: " << void_pointer << std::endl;
+  std::cout << "*void_pointer: " << *void_pointer << std::endl;
+
+}
